e238f2ab8528bcfb25e097267f1baba42c507010
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-20 19:38:59 +0300

    Initial Commit

---
+++ ScreenshotHide/ScreenshotHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotHideApp.swift
+//  ScreenshotHide
+//
+//  Created by Yunus Emre Taşdemir on 20.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
