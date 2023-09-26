1eeea2c32b6a818f56d4303e39728ab77f100e4e
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-26 14:29:07 +0300

    Building Screenshot Prevent View.

--- ScreenshotHide/ContentView.swift
+++ ScreenshotHide/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                NavigationLink {
+                    ScreenshitPreventView {
+                        GeometryReader {
+                            let size = $0.size
+                            
+                            Image(.pic)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: size.width, height: size.height)
+                                .clipShape(.rect(topLeadingRadius: 55, bottomTrailingRadius: 35))
+                        }
+                        .padding(15)
+                    }
+                    .navigationTitle ("IJustine")
+                } label: {
+                    Text("Show Image")
+                }
+                
+                NavigationLink {
+                    List {
+                        Section("API Key") {
+                            ScreenshitPreventView{
+                                Text("ITU98DHGMmak812KUGNW" )
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                        }
+                        
+                        Section("APNS Key") {
+                            ScreenshitPreventView{
+                                Text("enitsujil")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                        }
+                    }
+                    .navigationTitle("Key's")
+                } label: {
+                    Text("Show Security Keys!")
+                }
+            }
+            .navigationTitle("My List")
         }
-        .padding()
     }
 }
 
